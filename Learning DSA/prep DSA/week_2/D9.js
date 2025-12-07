00b8dc8b5555e0b404f885303806e7fafdfa6925
asgharali97 <asgharalifs48@gmail.com>
2025-12-08 03:14:38 +0500

    Day 9 done

---
+++ Learning DSA/prep DSA/week_2/D9.js
@@ -0,0 +1,23 @@
+// D9 of DSA 
+//Q 387. First Unique Character in a String
+// using hashmap
+let s = "leetcode"
+let map = new Map()
+function UChar() {
+
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i]
+        map.set(char, (map.get(char) || 0) + 1)
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        if (map.get(s[i]) === 1) {
+            return i
+            break
+        }
+    }
+    return -
+
+}
+
+console.log(UChar())
