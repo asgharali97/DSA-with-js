06d6723619d3409e1714b514b06cba419f1e6c50
asgharali97 <asgharalifs48@gmail.com>
2025-12-12 01:21:59 +0500

    Day 11 done

---
+++ Learning DSA/prep DSA/week_2/D11.js
@@ -0,0 +1,29 @@
+// Day 11 of DSA 
+// problem: Longest Repeating Character Replacement
+
+let s = "AABABBA"
+let k = 1
+
+function CharReplace(s,k){
+    let maxLen = 0
+
+    for(let left = 0; left < s.length; left++){
+        let freq = new Array(26).fill(0);
+
+        for(let right = left; right < s.length; right++){
+            let idx = s[right].charCodeAt(0) - 65
+            freq[idx]++
+            let windowLen = right - left + 1
+            let maxFreq = Math.max(...freq)
+
+            let needed = windowLen - maxFreq
+
+            if(needed <= k){
+                maxLen = Math.max(maxLen,windowLen)
+            }
+        }
+    }
+    return maxLen
+}
+
+console.log(CharReplace(s,k))
